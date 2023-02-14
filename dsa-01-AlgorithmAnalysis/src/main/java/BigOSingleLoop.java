b0f2b227b6876650d929c92ec726101798a55811
Kemail-terminal <sulkemref@gmail.com>
2023-02-13 19:40:40 -0600

    Single Nested Logarithmic complexity

--- dsa-01-AlgorithmAnalysis/src/main/java/BigOSingleLoop.java
+++ dsa-01-AlgorithmAnalysis/src/main/java/BigOSingleLoop.java
@@ -2,21 +2,32 @@ public class BigOSingleLoop {
 
     public static void main(String[] args) {
         long numberOfOperations=0;
-        int n=100;// size of  Data
+        int n=1000000;// size of  Data
+        int m=100;
         long startTime;
         long endTime;
         System.out.println("Input size n is = "+ n);
-        // Task 1 Single Loop...................
+        // Task 1 Single Loop................... O(n)
         startTime=System.currentTimeMillis();
         for (int i = 0; i < n ; i++) {
             numberOfOperations+=1;
         }
         endTime=System.currentTimeMillis();
 
+        System.out.println( "Single Loop Number of operations : "+numberOfOperations + " in "+ (endTime-startTime) +" milliseconds");
+
+        numberOfOperations=0;
         // Task 2 Nested Loops...................
+        startTime=System.currentTimeMillis();
+        for(int i = 0 ; i < n ; i++){
+            for (int j = 0; j < n; j++) {
+                numberOfOperations+=1;
+            }
+        }
+        endTime=System.currentTimeMillis();
 
+        System.out.println( "Nested Loop Number of operations : "+numberOfOperations + " in "+ (endTime-startTime) +" milliseconds");
         // Task 2 End...................
-
         // ......................................................................................................
         // Task 3 Three Nested Loops...................
 
@@ -38,5 +49,13 @@ public class BigOSingleLoop {
         // Task 4.5 End...................
 
         // Task 5 Logarithmic Complexity...................
+        numberOfOperations=0;
+        startTime=System.currentTimeMillis();
+        for (int i = 1; i < n ; i*=2) {
+            numberOfOperations+=1;
+        }
+        endTime=System.currentTimeMillis();
+        System.out.println( "Logarithmic Number of operations  : "+numberOfOperations + " in "+ (endTime-startTime) +" milliseconds");
+        // Task 5 End...................*/
     }
 }
